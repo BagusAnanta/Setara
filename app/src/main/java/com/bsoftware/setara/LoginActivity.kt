1aedfc5d7f43d4993a1a905e567ca36dfc7267f2
BagusAnanta <bagusananta278@gmail.com>
2024-01-07 23:20:05 +0700

    Login and SignUp Progress

--- app/src/main/java/com/bsoftware/setara/LoginActivity.kt
+++ app/src/main/java/com/bsoftware/setara/LoginActivity.kt
@@ -8,7 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.MailOutline
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -21,7 +28,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bsoftware.setara.ui.theme.SetaraTheme
@@ -49,6 +62,9 @@ fun LoginActivityView(){
     var email by remember{ mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
+    var passwordVisible by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -62,7 +78,17 @@ fun LoginActivityView(){
                 .padding(start = 20.dp, end = 20.dp, top = 5.dp),
             label = {
                 Text(text = stringResource(id = R.string.email_inputtext))
-            }
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Outlined.MailOutline,
+                    contentDescription = "EmailIcon"
+                )
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
         )
 
         // password field
@@ -74,6 +100,29 @@ fun LoginActivityView(){
                 .padding(start = 20.dp, end = 20.dp, top = 10.dp),
             label = {
                 Text(text = stringResource(id = R.string.password_inputtext))
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Outlined.Lock,
+                    contentDescription = "PasswordIcon"
+                )
+            },
+            visualTransformation = if(passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {focusManager.clearFocus()}
+            ),
+            trailingIcon = {
+                IconButton(
+                    onClick = {passwordVisible = !passwordVisible}
+                ) {
+                    Icon(
+                        painter = if(passwordVisible) painterResource(id = R.drawable.visible_outline_icon) else painterResource(id = R.drawable.visible_off_outline_icon),
+                        contentDescription = if(passwordVisible) "HidePassword" else "ShowPassword"
+                    )
+                }
             }
         )
 
