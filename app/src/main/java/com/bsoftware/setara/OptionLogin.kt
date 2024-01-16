9bcb2ceb2ceb3cd52fc9374ba01cf72018880eed
BagusAnanta <bagusananta278@gmail.com>
2024-01-16 09:05:06 +0700

    Option Login UI Update

--- app/src/main/java/com/bsoftware/setara/OptionLogin.kt
+++ app/src/main/java/com/bsoftware/setara/OptionLogin.kt
@@ -6,22 +6,39 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bsoftware.setara.ui.theme.SetaraTheme
 
 class OptionLogin : ComponentActivity() {
@@ -45,36 +62,105 @@ class OptionLogin : ComponentActivity() {
 fun OptionLoginView(){
     val context : Context = LocalContext.current
     val activity : Activity = OptionLogin()
-   Column(
-       verticalArrangement = Arrangement.Center,
-       horizontalAlignment = Alignment.CenterHorizontally
-   ) {
-            OutlinedButton(
-                onClick = {
-                    // login Activity
-                    context.startActivity(Intent(context,LoginActivity::class.java))
-                    activity.finish()
-                },
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.default_backgroud),
+            contentDescription = "Backgroud",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .matchParentSize()
+        )
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.option_login_asseth),
+                contentDescription = "OptionAsseth",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 5.dp)
-            ) {
-                Text(text = stringResource(id = R.string.login_text))
+                    .height(350.dp)
+            )
+
+            Text(
+                text = "Selamat",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    color = colorResource(id = R.color.blue_500),
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Text(
+                text = "Datang",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    color = colorResource(id = R.color.blue_500),
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 50.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                
+            ){
+                OutlinedButton(
+                    onClick = {
+                        // login Activity
+                        context.startActivity(Intent(context,LoginActivity::class.java))
+                        activity.finish()
+                    },
+                    modifier = Modifier
+                        .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 5.dp)
+                        .size(150.dp, 50.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.blue_500)
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.login_text),
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            fontSize = 18.sp
+                        )
+                    )
+                }
+
+                TextButton(
+                    onClick = {
+                        // SignUp Activity
+                        context.startActivity(Intent(context,SignupActivity::class.java))
+                        activity.finish()
+                    },
+                    modifier = Modifier
+                        .padding(start = 10.dp, end = 10.dp, top = 5.dp)
+                        .size(150.dp, 50.dp),
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.signup_text),
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            fontSize = 18.sp
+                        )
+                    )
+                }
             }
-       
-           OutlinedButton(
-               onClick = {
-                    // SignUp Activity
-                    context.startActivity(Intent(context,SignupActivity::class.java))
-                    activity.finish()
-               },
-               modifier = Modifier
-                   .fillMaxWidth()
-                   .padding(start = 10.dp, end = 10.dp, top = 5.dp)
-           ) {
-                Text(text = stringResource(id = R.string.signup_text))
-           }
-   }
+        }
+    }
 
 }
 
